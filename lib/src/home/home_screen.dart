15497a20724f3ba8bc326e5e5303333d036b6d6b
unknown <41454237+ollinmagno@users.noreply.github.com>
2021-05-26 18:48:54 -0300

    ajuste no botão e espaçamento

--- lib/src/home/home_screen.dart
+++ lib/src/home/home_screen.dart
@@ -15,12 +15,11 @@ class HomeScreen extends StatelessWidget {
                 borderRadius: BorderRadius.circular(20.0),
               ),
               child: Container(
-                width: double.infinity,
+                margin: const EdgeInsets.only(
+                    left: 26.0, right: 26.0, top: 22.0, bottom: 8.0),
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(20.0),
                 ),
-                margin: const EdgeInsets.only(
-                    left: 26.0, right: 26.0, top: 8.0, bottom: 8.0),
                 child: Column(
                   children: [
                     Row(
@@ -34,6 +33,7 @@ class HomeScreen extends StatelessWidget {
                               fontWeight: FontWeight.bold),
                         ),
                         IconButton(
+                          alignment: Alignment.centerRight,                          
                           icon: Icon(Icons.more_vert),
                           color: Colors.grey.shade400,
                           onPressed: () {},
@@ -68,68 +68,77 @@ class HomeScreen extends StatelessWidget {
                     Container(
                       child: Column(
                         children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                "Rentabilidade/mês",
-                                style: TextStyle(
-                                    color: Colors.grey.shade400,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                "2,767%",
-                                style: TextStyle(
-                                    color: Color(0xff3b5cb8),
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Rentabilidade/mês",
+                                  style: TextStyle(
+                                      color: Colors.grey.shade400,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "2,767%",
+                                  style: TextStyle(
+                                      color: Color(0xff3b5cb8),
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
                           ),
                           SizedBox(
                             height: 10.0,
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                "CDI",
-                                style: TextStyle(
-                                    color: Colors.grey.shade400,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                "3,45%",
-                                style: TextStyle(
-                                    color: Color(0xff3b5cb8),
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
+                          Padding(
+                            padding: const EdgeInsets.all(8),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "CDI",
+                                  style: TextStyle(
+                                      color: Colors.grey.shade400,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "3,45%",
+                                  style: TextStyle(
+                                      color: Color(0xff3b5cb8),
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
                           ),
                           SizedBox(
                             height: 10.0,
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text(
-                                "Ganho/mês",
-                                style: TextStyle(
-                                    color: Colors.grey.shade400,
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                              Text(
-                                "R\$ 1833,23",
-                                style: TextStyle(
-                                    color: Color(0xff3b5cb8),
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ],
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Ganho/mês",
+                                  style: TextStyle(
+                                      color: Colors.grey.shade400,
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  "R\$ 1833,23",
+                                  style: TextStyle(
+                                      color: Color(0xff3b5cb8),
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
                           ),
                           SizedBox(
                             height: 20.0,
@@ -138,34 +147,41 @@ class HomeScreen extends StatelessWidget {
                       ),
                     ),
                     Divider(),
-                    ButtonTheme(
-                      child: ButtonBar(
-                        children: <Widget>[
-                          Container(
-                            height: 40,
-                            width: 110,
-                            decoration: BoxDecoration(
-                              border: Border.all(
-                                color: Color(0xff3b5cb8),
+                    
+                    Row(
+                      //mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Spacer(),
+                        ButtonTheme(
+                          child: ButtonBar(
+                            children: <Widget>[
+                              Container(
+                                height: 35,
+                                width: 110,
+                                decoration: BoxDecoration(
+                                  border: Border.all(
+                                    color: Color(0xff3b5cb8),
+                                  ),
+                                  borderRadius: BorderRadius.circular(24.0),
+                                ),
+                                child: FlatButton(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20.0),
+                                  ),
+                                  child: const Text(
+                                    'VER MAIS',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 16,
+                                        color: Color(0xff3b5cb8)),
+                                  ),
+                                  onPressed: () {},
+                                ),
                               ),
-                              borderRadius: BorderRadius.circular(24.0),
-                            ),
-                            child: FlatButton(
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(20.0),
-                              ),
-                              child: const Text(
-                                'VER MAIS',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.w900,
-                                    fontSize: 16,
-                                    color: Color(0xff3b5cb8)),
-                              ),
-                              onPressed: () {},
-                            ),
+                            ],
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
