3223b58dd606149e519b206648163df032902d04
unknown <41454237+ollinmagno@users.noreply.github.com>
2021-05-26 19:14:43 -0300

    componente para cor

---
+++ lib/src/components/text_home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TextHome extends StatelessWidget {
+  final String text;
+  final Color color;
+
+  TextHome(this.text, {this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text,
+        style: TextStyle(
+            fontSize: 20,
+            color: this.color ?? Colors.grey.shade400,
+            fontWeight: FontWeight.bold));
+  }
+}
