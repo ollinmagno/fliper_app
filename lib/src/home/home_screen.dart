3b476e2a38bc8f5e07d933b70c6c60d1ddaec7b8
unknown <41454237+ollinmagno@users.noreply.github.com>
2021-05-27 21:48:50 -0300

    classe stateful

--- lib/src/home/home_screen.dart
+++ lib/src/home/home_screen.dart
@@ -1,7 +1,12 @@
 import 'package:fliper_app/src/components/text_home.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
